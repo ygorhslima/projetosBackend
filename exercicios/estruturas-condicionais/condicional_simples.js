d079938cb14144085c98e7242ac99c3dc99e697c
Ygor Henrique <ygorlima.dev06@gmail.com>
2025-11-20 10:57:35 -0300

    .

--- exercicios/estruturas-condicionais/condicional_simples.js
+++
@@ -1,9 +0,0 @@
-import PromptSync from "prompt-sync";
-const prompt = PromptSync()
-
-
-let num = Number(prompt("digite um número inteiro: "));
-if(num%2 == 0){
-    let raiz = Math.sqrt(num);
-    console.log(`a raiz quadrada de ${num} é ${raiz}`);
-}
--- exercicios/estruturas-condicionais/problema1.js
+++
@@ -1,9 +0,0 @@
-import PromptSync from "prompt-sync";
-const prompt = PromptSync()
-
-let num = Number(prompt("digite um valor: "));
-if(num % 5==0){
-    console.log(`${num} é divisível por 5`);
-}else{
-    console.log(`${num} não é divisível por 5`)
-}
